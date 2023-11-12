6658160be3612788558e00858818b315e05c0204
Billy <billy.wang.aus@gmail.com>
2023-11-12 11:28:41 +1100

    git init

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        // database table name: Activities
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
+
