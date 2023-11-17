935098ca8a25d0e45bf776f7dbbbe851a341e128
Billy <billy.wang.aus@gmail.com>
2023-11-17 17:05:51 +1100

    create Query handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,22 +9,22 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _dataContext;
-        public ActivitiesController(DataContext dataContext)
+        private readonly IMediator _mediator;
+        public ActivitiesController(IMediator mediator)
         {
-            _dataContext = dataContext;
+            _mediator = mediator;
         }
         
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _dataContext.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
         
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _dataContext.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
