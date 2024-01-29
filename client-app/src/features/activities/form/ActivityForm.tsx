3f0b51a14459676da8937101ba1caeb4ee7b8368
Billy <billy.wang.aus@gmail.com>
2024-01-29 12:12:17 +0800

    Cleaning up unused code

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useEffect, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
-import { useHistory, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { v4 as uuid } from 'uuid';
 
@@ -71,17 +71,17 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Form onSubmit={handleSubmit} autoComplete="off">
-                <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange} />
-                <Form.TextArea placeholder="Description" value={activity.description} name="description" onChange={handleInputChange} />
-                <Form.Input placeholder="Category" value={activity.category} name="category" onChange={handleInputChange} />
-                <Form.Input placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} type='date' />
-                <Form.Input placeholder="City" value={activity.city} name="city" onChange={handleInputChange} />
-                <Form.Input placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange} />
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
+                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
+                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
+                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} type='date' />
+                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
 
                 <Button.Group widths={2}>
-                    <Button loading={loading} positive type="submit" content="Save"></Button>
-                    <Button type="button" content="Cancel"></Button>
+                    <Button loading={loading} positive type='submit' content='Save'></Button>
+                    <Button as={Link} to='/activities' type='button' content='Cancel'></Button>
                 </Button.Group>
             </Form>
         </Segment>
