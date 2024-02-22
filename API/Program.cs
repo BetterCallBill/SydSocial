6a53b606ae515c8b11d7eaf31d33fe087c9fe7f4
Billy <billy.wang.aus@gmail.com>
2024-02-23 10:44:28 +1100

    Adding seed users

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -17,8 +19,9 @@ namespace API
             {
                 // create database if not exist
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
