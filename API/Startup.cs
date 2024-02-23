9aa69d8a40cd7d8e20eb52c7510cc01e4599bf92
Billy <billy.wang.aus@gmail.com>
2024-02-23 17:48:17 +1100

    Create an auth policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using Application.Activities;
 using Application.Middleware;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -17,6 +19,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // add policy to every endpoint
+            services.AddControllers(opt => 
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            });
             services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
             services.AddValidatorsFromAssemblyContaining<Create>();
             services.AddApplicationServices(_config);
