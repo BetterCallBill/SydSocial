16961c536ea1780ee9cfb8ad8b55028f9fc807a0
Billy <billy.wang.aus@gmail.com>
2024-02-28 10:37:52 +1100

    configure the new relationship, add infrastructure project

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,9 +9,28 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        
+
         // database table name: Activities
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // config joint table
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
 
