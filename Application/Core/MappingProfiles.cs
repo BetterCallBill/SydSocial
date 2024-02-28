875a1ca4f71fdcec6b619285761a39f44dfd214d
Billy <billy.wang.aus@gmail.com>
2024-02-28 15:09:11 +1100

    config automapper

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -10,7 +10,14 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
-            CreateMap<Activity, ActivityDto>();
+            
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
+                    .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+                    
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(p => p.Username, o => o.MapFrom(aa => aa.AppUser.UserName))
+                .ForMember(p => p.DisplayName, o => o.MapFrom(aa => aa.AppUser.DisplayName));
         }
     }
 }
