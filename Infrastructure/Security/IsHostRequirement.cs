fcea57402e50ca36bb379903a8621257ce94a07f
Billy <billy.wang.aus@gmail.com>
2024-02-28 17:22:15 +1100

    resolve the bug with with the edit handler

--- Infrastructure/Security/IsHostRequirement.cs
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security
@@ -29,7 +30,11 @@ namespace Infrastructure.Security
             // get activity id from request
             var activityId = Guid.Parse(_httpContextAccessor.HttpContext?.Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value?.ToString());
             
-            var attendee = _dbContext.ActivityAttendees.FindAsync(userId, activityId).Result;
+            // AsNoTracking
+            var attendee = _dbContext.ActivityAttendees
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.AppUserId == userId && x.ActivityId == activityId)
+                .Result;
             
             if (attendee == null) return Task.CompletedTask;
             
