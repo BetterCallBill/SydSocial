435e0ebc91a58d2a63498ddb86e994527fe01ebb
Billy <billy.wang.aus@gmail.com>
2024-03-02 19:07:39 +1100

    add the Photo entity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -6,8 +6,11 @@ namespace Domain
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
+        public string Bio { get; set; }
         
         // add many to many relations
         public ICollection<ActivityAttendee> Activities { get; set; }
+        
+        public ICollection<Photo> Photos { get; set; }
     }
 }
