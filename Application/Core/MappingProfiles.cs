8c11c1f2d23f41303d74be033f6b0841a74658cc
Billy <billy.wang.aus@gmail.com>
2024-03-03 10:35:18 +1100

    add an attendee DTO

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,9 +15,11 @@ namespace Application.Core
                 .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
                     .FirstOrDefault(x => x.IsHost).AppUser.UserName));
 
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(d => d.Username, o => o.MapFrom(aa => aa.AppUser.UserName))
-                .ForMember(d => d.DisplayName, o => o.MapFrom(aa => aa.AppUser.DisplayName));
+                .ForMember(d => d.DisplayName, o => o.MapFrom(aa => aa.AppUser.DisplayName))
+                .ForMember(d => d.Bio, o => o.MapFrom(aa => aa.AppUser.Bio))
+                .ForMember(d => d.Image, o => o.MapFrom(p => p.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
 
             // public string Image { get; set; } => p.Photos.FirstOrDefault(x => x.IsMain).Url
             CreateMap<AppUser, Profiles.Profile>()
