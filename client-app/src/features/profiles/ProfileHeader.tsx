1d48a18d997939cca333f3948dd5b44227d7dc41
Billy <billy.wang.aus@gmail.com>
2024-03-04 14:23:28 +1100

    add the profile content

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -1,16 +1,21 @@
 import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
+import { Profile } from '../../app/models/profile';
 
-export default observer(function ProfileHeader() {
+interface Props {
+    profile: Profile;
+}
+
+export default observer(function ProfileHeader({ profile }: Props) {
     return (
         <Segment>
             <Grid>
                 <Grid.Column width={12}>
                     <Item.Group>
                         <Item>
-                            <Item.Image avatar size="small" src='/assets/user.png' />
+                            <Item.Image avatar size="small" src={profile.image || '/assets/user.png'} />
                             <Item.Content verticalAlign="middle">
-                                <Header as="h1" content='username' />
+                                <Header as="h1" content={profile.displayName} />
                             </Item.Content>
                         </Item>
                     </Item.Group>
