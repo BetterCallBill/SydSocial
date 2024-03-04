ddd7a39da589fc2eaabb4349f34505ce7a38f0cb
Billy <billy.wang.aus@gmail.com>
2024-03-04 20:06:07 +1100

    add a photo upload widget

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,22 +1,48 @@
 import { observer } from 'mobx-react-lite';
-import { Card, Header, Tab, Image } from 'semantic-ui-react';
+import { Card, Header, Tab, Image, Grid, Button } from 'semantic-ui-react';
 import { Profile } from '../../app/models/profile';
+import { useStore } from '../../app/stores/store';
+import { useState } from 'react';
+import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget';
 
 interface Props {
     profile: Profile;
 }
 
 export default observer(function ProfilePhotos({ profile }: Props) {
+    const {
+        profileStore: { isCurrentUser },
+    } = useStore();
+    const [addPhotoModel, setAddPhotoModel] = useState(false);
+
     return (
         <Tab.Pane>
-            <Header icon="image" content="Photos" />
-            <Card.Group itemsPerRow={5}>
-                {profile.photos?.map(photo => (
-                    <Card key={photo.id}>
-                        <Image src={photo.url} />
-                    </Card>
-                ))}
-            </Card.Group>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated="left" icon="image" content="Photos" />
+                    {isCurrentUser && (
+                        <Button
+                            floated="right"
+                            basic
+                            content={addPhotoModel ? 'Cancel' : 'Add Photo'}
+                            onClick={() => setAddPhotoModel(!addPhotoModel)}
+                        />
+                    )}
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    {addPhotoModel ? (
+                        <PhotoUploadWidget />
+                    ) : (
+                        <Card.Group itemsPerRow={5}>
+                            {profile.photos?.map(photo => (
+                                <Card key={photo.id}>
+                                    <Image src={photo.url} />
+                                </Card>
+                            ))}
+                        </Card.Group>
+                    )}
+                </Grid.Column>
+            </Grid>
         </Tab.Pane>
     );
 });
